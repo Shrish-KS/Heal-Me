e553a603bfd9634db8480344a4d7b224f1f8d9e5
Shrish KS <shrishdks@gmail.com>
2023-11-05 07:49:29 +0530

    Added signin feature

--- routers/auth.js
+++ routers/auth.js
@@ -4,25 +4,48 @@ const bcrypt = require("bcrypt")
 
 const router = express.Router()
 
-router.post('/register',async(req,res) => {
+router.get('/', (req,res) => {
+    res.status(200),send("Welcome to Authorisation Router")
+})
+
+router.post('/signup',async(req,res) => {
     try{
+        //adding encryption to password
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(req.body.password, salt)
         
+        //create new user
         const user = await new User({
             username: req.body.username,
             email: req.body.email,
             password: hashedPassword
         })
 
+        // save user
         user.save()
         return res.status(200).json(user)
     }
     catch(err){
         return res.status(409).json({success:false,error:err})
     }
+})
 
-    return res.send("Hey, It is auth route")
+router.post('/login', async(req,res) => {
+    try{
+        const user =await User.findOne({email: req.body.email})
+        if(!user){
+            return res.status(404).json("User Not Found")
+        }
+        const valid_pass = await bcrypt.compare(req.body.password, user.password)
+        if(!valid_pass){
+            return res.status(400).json("Wrong password")
+        }
+
+        return res.status(200).json(user)
+    }
+    catch(err){
+        res.status(404).json({success:false,error:err})
+    }
 })
 
 module.exports = router
