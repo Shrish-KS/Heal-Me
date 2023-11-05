68b7d187b2fe97c22f315ae340023297fb87b19e
Shrish KS <shrishdks@gmail.com>
2023-11-05 15:56:11 +0530

    Added put request for user

--- routers/users.js
+++ routers/users.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const User = require('../models/users.js')
+const Info = require('../models/info.js')
+const bcrypt = require("bcrypt")
 
 const router = express.Router()
 
@@ -6,7 +9,30 @@ router.get('/',(req,res) => {
     res.send("Hey, It is user route")
 })
 
-
+router.put('/:id',async(req,res) => {
+    if(req.body.UserID === req.params.id){
+        try{
+            if(req.body.password){
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password,salt)
+            }
+            const user =await User.findByIdAndUpdate(req.params.id,{
+                $set: req.body
+            })
+            const info =await Info.findByIdAndUpdate(user.info,{
+                $set:req.body
+            })
+            res.status(200).json({user,info})
+        }
+        catch(err){
+            console.log(err)
+            res.status(400).json({success:false, error:err})
+        }
+    }
+    else{
+        res.status(403).json("You can edit only your account")
+    }
+})
 
 
 module.exports = router
