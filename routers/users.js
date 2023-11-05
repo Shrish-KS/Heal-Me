4c5ca10b4fa33f41cc6e40a680d151a691328512
Shrish KS <shrishdks@gmail.com>
2023-11-05 16:08:48 +0530

    Added delete request for user

--- routers/users.js
+++ routers/users.js
@@ -10,7 +10,7 @@ router.get('/',(req,res) => {
 })
 
 router.put('/:id',async(req,res) => {
-    if(req.body.UserID === req.params.id){
+    if(req.body.UserID === req.params.id || req.user.isAdmin){
         try{
             if(req.body.password){
                 const salt = await bcrypt.genSalt(10);
@@ -22,7 +22,7 @@ router.put('/:id',async(req,res) => {
             const info =await Info.findByIdAndUpdate(user.info,{
                 $set:req.body
             })
-            res.status(200).json({user,info})
+            res.status(200).json({success:true,user,info})
         }
         catch(err){
             console.log(err)
@@ -34,5 +34,24 @@ router.put('/:id',async(req,res) => {
     }
 })
 
+
+router.delete('/:id',async(req,res) => {
+    if(req.body.UserID === req.params.id || req.user.isAdmin){
+        try{
+            const user=await User.findById(req.params.id)
+            await Info.deleteOne(user.info)
+            await User.deleteOne(user)
+
+            res.status(200).json({success:true,msg:"Deleted Successfully"})
+        }
+        catch(err){
+            console.log(err)
+            res.status(400).json({success:false, error:err})
+        }
+    }
+    else{
+        res.status(403).json("You can delete only your account")
+    }
+})
 
 module.exports = router
